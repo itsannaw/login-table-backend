6fda6924aaa014d906236350d54d312c837e8ea1
itsannaw <emikot321@gmail.com>
2023-10-11 00:03:23 +0300

    added cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,4 +14,12 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       methods: :any, #[:get, :post, :put, :patch, :delete, :options, :head]
       expose: [:Authorization]
   end
+
+  allow do
+    origins "https://login-table.vercel.app/"
+    resource "*",
+      headers: :any,
+      methods: :any, #[:get, :post, :put, :patch, :delete, :options, :head]
+      expose: [:Authorization]
+  end
 end
